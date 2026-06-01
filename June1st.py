5a39d6e70e50ee57b85019c95371f3006d290d07
LOVETHULIONG\lovet <Lovetuliong@gmail.com>
2026-06-01 14:02:05 -0600

    message you want in your commit

--- June1st.py
+++ June1st.py
@@ -1,1 +1,3 @@
 print("Hello GIT Hub, this is my first commit!")
+print("I am learning how to use GIT Hub for version control.")
+print("This is a great way to collaborate on projects and keep track of changes.")
